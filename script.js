10d08d3215cb7c5ef5ef63cc1dc80a9387c5e8bc
georgekilauridze <99383455+georgekilauridze@users.noreply.github.com>
2022-03-07 11:57:08 +0400

    Lesson11

---
+++ script.js
@@ -0,0 +1,66 @@
+
+// let user = {
+//     firstname: 'giorgi',
+//     lastname: 'smith',
+//     age: 25,
+//     studentstatus: 'active'
+// }
+
+// console.log(user['studentstatus']);
+
+// let numbers = [4, 6, 12, 23, 27, 28, 3];
+
+// console.log(numbers[0]);
+// console.log(numbers[4]);
+// console.log(numbers[2]);
+// console.log(numbers[1]);
+// console.log(numbers[3]);
+// console.log(numbers[5]);
+
+// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+
+// for (let x = 0; x < numbers.length; x++) {
+//     if (numbers[x] > 8) {
+//     console.log(numbers[x]);    
+//     }
+// }
+
+// let numbers = {
+//     name: 'giorgi',
+//     age: 20,
+//     studentstatus: 'active'
+// }
+// let number = 20;
+
+// if (number > 19) {
+//     console.log('hello');
+// }
+// let number = 'giorgi'
+// if (user = 'giorgi') {
+//     console.log('hello giorgi');
+// }
+
+// if (user = 'active') {
+//        console.log('hello world');
+// } else if (age < 25) {
+//        console.log('hello world');
+// }
+// let number = 20;
+// if (number < 20) {
+//     console.log('hello');
+// } else {
+//     console.log('error')
+// }
+       
+// let array = ['watermelon', 'pear', 10, 45, 50, 'apple', 'ananas'];
+//      console.log(array[5]);
+//      console.log(array[6]);
+//      console.log(array[0]);
+//      console.log(array[1]);
+
+let users =  [
+    {username:'test1', status: false},
+    {username:'test2', status: false},
+    {username:'test3', status: true}
+];
+console.log('test3');
